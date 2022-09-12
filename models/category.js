e662a854e45c4969a2afb3cba560ca8b3828aa29
jesusi3 <js508102@gmail.com>
2022-09-12 18:43:03 -0500

    order History styling

--- models/category.js
+++ models/category.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// require('./mainCategory');
 
 
 const categorySchema = new Schema({
--- src/components/HistoryLineItem/HistoryLineItem.jsx
+++ src/components/HistoryLineItem/HistoryLineItem.jsx
@@ -1,10 +1,12 @@
-
+import './HistoryLineItem.css';
 
 export default function HistoryLineItem({order}) {
 
     return (
         <>  
-            <h1>{order.item._id}</h1>
+        <div className="line-item">
+            <h4>{order.item._id}</h4>
+            <h4>{order.item.name}</h4></div>
         </>
     );
 }
